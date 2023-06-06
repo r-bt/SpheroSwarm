daaac97053083391e3ca30b7ae55af484ce07efe
Richard Beattie <richard.beattie91@gmail.com>
2023-06-06 12:14:16 -0400

    Make async to increase speed

--- spherov2_test/main.py
+++ spherov2_test/main.py
@@ -4,55 +4,48 @@ from contextlib import ExitStack
 from spherov2.types import Color
 import asyncio
 
-# import time
 
-toys = scanner.find_toys()
-bots = []
-
-print(toys)
-
-
-async def set_matrix(bot, color):
+def set_matrix(bot, color):
     bot.set_matrix_fill(
         2, 0, 6, 6, Color(r=int(color[0]), g=int(color[1]), b=int(color[2]))
     )
 
 
+def connect_to_sphero(stack, toy, index):
+    attempts = 0
+    while attempts < 3:
+        print(
+            "Connecting to sphero {index}, attempt {attempt}".format(
+                index=index, attempt=attempts + 1
+            )
+        )
+        try:
+            bots.append(stack.enter_context(SpheroEduAPI(toy)))
+            print("Connected to sphero {index}".format(index=index))
+            return
+        except:
+            print(
+                "Somethine went wrong with sphero {index}, retyring".format(index=index)
+            )
+            attempts += 1
+
+
 async def main():
+    pass
     with ExitStack() as stack:
-        i = 0
-        while i < min(len(toys), 10):
-            try:
-                print(i)
-                bots.append(stack.enter_context(SpheroEduAPI(toys[i])))
-                i += 1
-            except:
-                print("Something went wrong with toy:{toy}".format(toy=toys[i]))
+        await asyncio.gather(
+            *[asyncio.to_thread(connect_to_sphero, stack, toys[i], i) for i in range(7)]
+        )
         while True:
             color = input("Color?")
             color = color.split(",")
-            await asyncio.gather(*[set_matrix(bot, color) for bot in bots])
+            await asyncio.gather(
+                *[asyncio.to_thread(set_matrix, bot, color) for bot in bots]
+            )
 
+
+toys = scanner.find_toys()
+bots = []
+print(toys)
 
 asyncio.run(main())
-# bots = []
-
-# for toy in toys:
-#     bots.append(SpheroEduAPI(toy))
-
-# for bot in bots:
-#     bot.set_matrix_fill(2, 0, 6, 6, Color(r=0, g=255, b=255)) #Set Matrix Box
-
-# for toy in toys:
-#     pass
-
-
-# if "3026" in toy:
-#     print(toy)
-
-# toy = scanner.find_toy()
-# with SpheroEduAPI(toy) as api:
-#     api.set_matrix_fill(2, 0, 6, 6, Color(r=0, g=255, b=255)) #Set Matrix Box
-#     api.set_speed(60)
-#     time.sleep(2)
-#     api.set_speed(0)
