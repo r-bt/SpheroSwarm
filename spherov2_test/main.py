1269dd0225655b2f696585170f433a34e8ddde5e
Richard Beattie <richard.beattie91@gmail.com>
2023-06-28 09:36:28 -0400

    Use async spherov2

--- spherov2_test/main.py
+++ spherov2_test/main.py
@@ -4,70 +4,68 @@ from spherov2.adapter.bleak_adapter import BleakAdapter
 from spherov2.sphero_edu import SpheroEduAPI
 from contextlib import AsyncExitStack
 from spherov2.types import Color
-# from bleak import BleakScanner
 import asyncio
 import pdb
 import datetime
 
+def print_log(data):
+    print(data)
+
 async def connect_to_sphero(stack, name, index):
     # Get adapter
-    adapter = "hci{index}".format(index=index%3)
-    # Find the toy
-    try:
-        toy = await scanner.find_toy(toy_name=name, bleak_adapter=adapter)
-    except Exception as e:
-        print(e)
-        return
-    # Connect to the toy
+    adapter = "hci{index}".format(index=index % 3)
     attempts = 0
     while attempts < 3:
-        print(
+        try:
+            print_log("Finding toy...")
+            toy = await scanner.find_toy(toy_name=name, bleak_adapter=adapter)
+            print_log(
             "Connecting to sphero {name} (#{index}), attempt {attempt}, using {adapter}".format(
                 name=toy.name, index=index, attempt=attempts + 1, adapter=adapter
-            )
-        )
-        try:
+            ))
             bots.append(await stack.enter_async_context(SpheroEduAPI(toy)))
-            print("Connected to sphero {name} (#{index})".format(name=toy.name, index=index))
+            print_log("Connected to sphero {name} (#{index})".format(name=toy.name, index=index))
             return
         except Exception as e:
-            print(
-                "Something went wrong with sphero {name} (#{index}), retrying".format(name=toy.name, index=index)
+            print_log(
+                "Something went wrong with sphero {name} (#{index}), retrying".format(name=name, index=index)
             )
-            print(e)
+            print_log(e)
             attempts += 1
 
-async def set_bots_matrix(color):
-    print(datetime.datetime.now())
-    background_tasks = set()
-    async with asyncio.TaskGroup() as tg:
-        for bot in bots:
-            task = tg.create_task(bot.set_matrix_fill(2, 0, 6, 6, Color(r=int(color[0]), g=int(color[1]), b=int(color[2]))))
-            background_tasks.add(task)
-            task.add_done_callback(background_tasks.discard)
-    print(datetime.datetime.now())
+colors = [(255,0,0), (0,255,0), (0,0,255), (255,0,255), (0,255,255)]
 
+async def set_bots_matrix(color):
+    await asyncio.gather(*(bot.set_matrix_fill(0, 0, 7, 7, Color(r=int(colors[i][0]), g=int(colors[i][1]), b=int(colors[i][2]))) for i, bot in enumerate(bots)))
+
+async def all_spin():
+    await asyncio.gather(*(bot.spin(1080, 2) for bot in bots))
 
 async def main():
     async with AsyncExitStack() as stack:
-        for i in range(min(len(sphero_names), 15)):
+        for i in range(min(len(sphero_names), count)):
                 await connect_to_sphero(stack, sphero_names[i], i)
+        should_quit = False
+        await set_bots_matrix((0,0,0))
+        for bot in bots:
+            bot.set_speed(50)
         while True:
-            color = input("Color?")
-            color = color.split(",")
-            await set_bots_matrix(color)
+            wait = input("Color")
+            await all_spin()
+        # while not should_quit:
+        #     color = input("Color?")
+        #     if color == "quit":
+        #         should_quit = True  
+        #     else:
+        #         color = color if color != '' else '255,0,0'
+        #         color = color.split(",")
+        #         await set_bots_matrix(color)
 
-sphero_names = ["SB-1B35", "SB-2175", "SB-3026", "SB-618E", "SB-6B58", "SB-9938", "SB-BFD4", "SB-C1D2", "SB-CEFA", "SB-DF1D", "SB-F465", "SB-F479", "SB-F885", "SB-FCB2"]
+sphero_names = ["SB-1B35", "SB-F860", "SB-2175", "SB-3026", "SB-618E", "SB-6B58", "SB-9938", "SB-BFD4", "SB-C1D2", "SB-CEFA", "SB-DF1D", "SB-F465", "SB-F479", "SB-F885", "SB-FCB2"]
+count = 5
 
-print("Sphero Swarm! Beginning to connect to {count} spheros".format(count=len(sphero_names)))
-
-# toys = scanner.find_toys()
 bots = []
-# print(sorted(toys, key=lambda x: x.name))
-# if len(toys) != 14:
-    # print("Spheros missing!")
-# else:
 
-# main()
+print_log("Sphero Swarm! Beginning to connect to {count} spheros".format(count=count))
 
 asyncio.run(main())
--- spherov2_test/minimal_connection.py
+++ spherov2_test/minimal_connection.py
@@ -1,5 +1,6 @@
 import asyncio
 import bleak
+import pdb
 
 handshake = [('00020005-574f-4f20-5370-6865726f2121', bytearray(b'usetheforce...band'))]
 response_uuid = '00010002-574f-4f20-5370-6865726f2121'
@@ -7,16 +8,34 @@ send_uuid = '00010002-574f-4f20-5370-6865726f2121'
 
 wake_command = b'\x8d\n\x13\r\x00\xd5\xd8'
 
+saw_start = False
+saw_end = False
+
+sensors = ["accel_one", 'accelerometer', 'ambient_light', 'attitude', "core_time", 'gyroscope', 'locator', "quaternion", 'velocity']
+
 async def main():
     # Find devices
 
     bots = []
 
-    for index, sphero in enumerate(sphero_names):
+    for index, sphero in enumerate(sphero_names[:10]):
 
-        adapter = "hci1" if index % 2 else "hci0"
+        adapter = "hci{index}".format(index=index % 3)
 
         device = await bleak.BleakScanner.find_device_by_name(sphero, 10.0, adapter=adapter)
+
+        pdb.set_trace()
+
+        def api_read(char, data: bytearray):
+            """
+            Callback for bleak
+
+            :param char - characteristic 
+            :param data - bytearray of the recieved data
+            """
+            with open("{name}.txt".format(name=sphero), "a") as file:
+                file.write(','.join(format(x, '02x') for x in data))
+                file.write("\n")
 
         bot = bleak.BleakClient(device, timeout=5.0)
         try:
@@ -27,7 +46,7 @@ async def main():
             # Perform handshake
             for uuid, data in handshake:
                 await bot.write_gatt_char(uuid, data, True)
-            await bot.start_notify(response_uuid, lambda x, y: None)
+            await bot.start_notify(response_uuid, api_read)
             print("Performed handshake")
             # Wake up the device
             await bot.write_gatt_char(send_uuid, wake_command, True)
@@ -40,6 +59,6 @@ async def main():
     for bot in bots:
         await bot.disconnect()
 
-sphero_names = ["SB-1B35", "SB-2175", "SB-3026", "SB-618E", "SB-6B58", "SB-9938", "SB-BFD4", "SB-C1D2", "SB-CEFA", "SB-DF1D", "SB-F465", "SB-F479", "SB-F885", "SB-FCB2"]
+sphero_names = ["SB-2175", "SB-3026", "SB-618E", "SB-6B58", "SB-9938", "SB-BFD4", "SB-C1D2", "SB-CEFA", "SB-DF1D", "SB-F465", "SB-F479", "SB-F885", "SB-FCB2"]
 
 asyncio.run(main())
