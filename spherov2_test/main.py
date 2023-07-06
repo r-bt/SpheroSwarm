7f2680a02ec141275abb61b35a8eea188a39eab4
Richard Beattie <richard.beattie91@gmail.com>
2023-07-06 11:57:59 -0400

    Lots of testing

--- spherov2_test/main.py
+++ spherov2_test/main.py
@@ -5,8 +5,7 @@ from spherov2.sphero_edu import SpheroEduAPI
 from contextlib import AsyncExitStack
 from spherov2.types import Color
 import asyncio
-import pdb
-import datetime
+import random
 
 def print_log(data):
     print(data)
@@ -20,8 +19,8 @@ async def connect_to_sphero(stack, name, index):
             print_log("Finding toy...")
             toy = await scanner.find_toy(toy_name=name, bleak_adapter=adapter)
             print_log(
-            "Connecting to sphero {name} (#{index}), attempt {attempt}, using {adapter}".format(
-                name=toy.name, index=index, attempt=attempts + 1, adapter=adapter
+            "Connecting to sphero {name} (#{index}) ({address}), attempt {attempt}, using {adapter}".format(
+                name=toy.name, index=index, attempt=attempts + 1, adapter=adapter, address=toy.address
             ))
             bots.append(await stack.enter_async_context(SpheroEduAPI(toy)))
             print_log("Connected to sphero {name} (#{index})".format(name=toy.name, index=index))
@@ -33,25 +32,31 @@ async def connect_to_sphero(stack, name, index):
             print_log(e)
             attempts += 1
 
-colors = [(255,0,0), (0,255,0), (0,0,255), (255,0,255), (0,255,255)]
-
-async def set_bots_matrix(color):
-    await asyncio.gather(*(bot.set_matrix_fill(0, 0, 7, 7, Color(r=int(colors[i][0]), g=int(colors[i][1]), b=int(colors[i][2]))) for i, bot in enumerate(bots)))
+async def set_bots_matrix(color = None):
+    if color:
+        await asyncio.gather(*(bot.set_matrix_fill(0, 0, 7, 7, Color(r=int(color[0]), g=int(color[1]), b=int(color[2]))) for i, bot in enumerate(bots)))
+    else:
+        tasks = []
+        for bot in bots:
+            tasks.append(bot.set_matrix_fill(0, 0, 7, 7, Color(r=int(random.randint(0,255)), g=int(random.randint(0,255)), b=int(random.randint(0,255)))))
+        await asyncio.gather(*tasks)
 
 async def all_spin():
-    await asyncio.gather(*(bot.spin(1080, 2) for bot in bots))
+    await asyncio.gather(*(bot.spin(1440, 2) for bot in bots))
 
 async def main():
     async with AsyncExitStack() as stack:
         for i in range(min(len(sphero_names), count)):
                 await connect_to_sphero(stack, sphero_names[i], i)
         should_quit = False
-        await set_bots_matrix((0,0,0))
+        await set_bots_matrix()
+        quit = input("quit?")
         for bot in bots:
-            bot.set_speed(50)
+            bot.set_speed(25)
         while True:
             wait = input("Color")
             await all_spin()
+
         # while not should_quit:
         #     color = input("Color?")
         #     if color == "quit":
@@ -62,7 +67,7 @@ async def main():
         #         await set_bots_matrix(color)
 
 sphero_names = ["SB-1B35", "SB-F860", "SB-2175", "SB-3026", "SB-618E", "SB-6B58", "SB-9938", "SB-BFD4", "SB-C1D2", "SB-CEFA", "SB-DF1D", "SB-F465", "SB-F479", "SB-F885", "SB-FCB2"]
-count = 5
+count = 1
 
 bots = []
 
